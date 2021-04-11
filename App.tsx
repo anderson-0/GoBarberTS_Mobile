69ce2da21030bdd42b0cd5b95e3ba80c06b69249
Anderson Mattjie da Silva <anderson.consultant.it@outlook.com>
2021-04-11 19:48:48 -0300

    feat: added StatusBar and changed backgroundColor to scale of grey

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
-import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "react-native";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-const App: React.FC = () => <View />;
+const App: React.FC = () => (
+  <>
+    <StatusBar barStyle="light-content" />
+    <View style={styles.container} />
+  </>
+);
 
 export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#312e38",
     alignItems: "center",
     justifyContent: "center",
   },
