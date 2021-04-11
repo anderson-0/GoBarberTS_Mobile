2ffe216bc1df68788a2bd088d0983a06ec019197
Anderson Mattjie da Silva <anderson.consultant.it@outlook.com>
2021-04-11 19:37:59 -0300

    feat: initial config

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const App: React.FC = () => <View />;
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
